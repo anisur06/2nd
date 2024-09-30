82f6f2038a986beab8a296fa939e7ab76bc64cf5
Anisur Rahman <anisurerahaman06@gmail.com>
2024-09-30 16:18:09 +0600

    n

---
+++ .vscode/int funCode() {.cpp
@@ -0,0 +1,4 @@
+int funCode() {
+    std::cout << "This is a simple C++ function!" << std::endl;
+    return 0;
+}
---
+++ Untitled-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+    srand(time(0));
+    number = rand() % 100 + 1; // Generates a random number between 1 and 100
+
+    printf("Guess the number (between 1 and 100):\n");
+
+    do {
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Lower!\n");
+        } else if (guess < number) {
+            printf("Higher!\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        }
+    } while (guess != number);
+
+    return 0;
+}
